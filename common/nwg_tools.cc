de0c03ebb95bf351ab8d8f8d9340b18004e0cfcd
Evo221 <mitsubishievo221@gmail.com>
2020-11-09 14:23:48 +0900

    Add foot terminal.

--- common/nwg_tools.cc
+++ common/nwg_tools.cc
@@ -102,6 +102,7 @@ std::string detect_wm() {
         // User may change the file content (terminal name) to their liking.
         std::string terms [] = {
             "alacritty -e",
+            "foot",
             "kitty -e",
             "urxvt -e",
             "lxterminal -e",
